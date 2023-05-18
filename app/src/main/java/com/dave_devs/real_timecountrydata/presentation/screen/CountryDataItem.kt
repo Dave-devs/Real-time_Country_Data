55c1c87112ce5c9e2dc8d77cae5b26c4dcfa8457
Oreoluwa <Odetundeoreoluwadavid@gmail.com>
2023-05-18 14:49:18 +0100

    Added CountryDataItem and some correction in CountryDataScreen

--- app/src/main/java/com/dave_devs/real_timecountrydata/presentation/screen/CountryDataItem.kt
+++ app/src/main/java/com/dave_devs/real_timecountrydata/presentation/screen/CountryDataItem.kt
@@ -1,8 +1,14 @@
 package com.dave_devs.real_timecountrydata.presentation.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.dave_devs.real_timecountrydata.domain.model.CountryData
 
 @Composable
@@ -11,6 +17,225 @@ fun CountryDataItem(
     countryData: CountryData
 ) {
     Column(modifier) {
-
+        Text(
+            text = countryData.name,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Alpha2Code: ${countryData.alpha2Code}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Alpha3Code: ${countryData.alpha3Code}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Calling Code: ${countryData.callingCodes}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Capital: ${countryData.capital}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Alt Spelling: ${countryData.altSpellings}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Region: ${countryData.region}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Sub-region: ${countryData.subregion}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Population: ${countryData.population}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Latitude/Longitude: ${countryData.latlng}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Demonym: ${countryData.demonym}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Area: ${countryData.area}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Gini: ${countryData.gini}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Timezone: ${countryData.timezones}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Borders: ${countryData.borders.size}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Native name: ${countryData.name}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Numeric code: ${countryData.numericCode}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Currencies: ${countryData.currencies}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        countryData.languages.forEach { lang ->
+            Text(
+                text = lang.name,
+                fontWeight = FontWeight.Light,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier.height(6.dp))
+            Text(
+                text = lang.nativeName,
+                fontWeight = FontWeight.Light,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier.height(6.dp))
+            Text(
+                text = lang.iso639_1,
+                fontWeight = FontWeight.Light,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier.height(6.dp))
+            Text(
+                text = lang.iso639_2,
+                fontWeight = FontWeight.Light,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Spacer(Modifier.height(10.dp))
+        //Translation
+        Text(
+            text = "Translations: ",
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(6.dp))
+        Text(
+            text = countryData.translations.br,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = countryData.translations.de,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = countryData.translations.es,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = countryData.translations.fr,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = countryData.translations.fa,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = countryData.translations.hr,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = countryData.translations.it,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = countryData.translations.pt,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = countryData.translations.nl,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = countryData.translations.ja,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(Modifier.height(10.dp))
+        countryData.regionalBlocs.forEach { regionalBloc -> 
+            Text(
+                text = regionalBloc.name,
+                fontWeight = FontWeight.Light,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier.height(6.dp))
+            Text(
+                text = regionalBloc.acronym,
+                fontWeight = FontWeight.Light,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = "Cioc: ${countryData.cioc}",
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.bodyMedium
+        )
     }
 }
--- app/src/main/java/com/dave_devs/real_timecountrydata/presentation/screen/CountryDataScreen.kt
+++ app/src/main/java/com/dave_devs/real_timecountrydata/presentation/screen/CountryDataScreen.kt
@@ -8,10 +8,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -24,8 +27,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.dave_devs.real_timecountrydata.R
 import kotlinx.coroutines.flow.collectLatest
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -61,23 +66,38 @@ fun CountryDataScreen(
                 TextField(
                     value = viewModel.searchQuery.value,
                     onValueChange = viewModel::onSearch,
-                    modifier.fillMaxWidth(),
-                    placeholder = { Text(text = "Search...") },
+                    modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    placeholder = { Text(text = "Type keyword...") },
                     maxLines = 1,
-                    singleLine = true
+                    singleLine = true,
+                    trailingIcon = {
+                        IconButton(
+                            onClick = { viewModel.searchQuery }
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_search_24),
+                                contentDescription = null
+                            )
+                        }
+                    }
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 LazyColumn(modifier.fillMaxSize()) {
-                    items(state.countryDataItems.size) {
-
+                    items(state.countryDataItems.size) {i ->
+                        val countryData = state.countryDataItems[i]
+                        if(i > 0 ) {
+                            Spacer(modifier.height(8.dp))
+                        }
+                        CountryDataItem(countryData = countryData)
+                        if(i < state.countryDataItems.size - 1) Divider()
                     }
-//                    if(state.isLoading) {
-//                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-//                    }
-
                 }
+            }
+            if(state.isLoading) {
+                CircularProgressIndicator(modifier.align(Alignment.Center))
             }
         }
     }
-    
 }
